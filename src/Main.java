3bd81ca2862c8ed02029934cf2aa6a4b913c57c2
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-08 12:50:38 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {                                                        //Testing field
+        Employee employee=new Employee("krishnanunni",11125,78,79,80);
+        System.out.println(employee.getResult());
+    }
+}
